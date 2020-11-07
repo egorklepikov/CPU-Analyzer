1ad94a5a348b96438a106f8f4c928eefb8e072ab
Egor.Klepikov <Egor.Klepikov@jetbrains.com>
2020-11-07 14:51:52 +0300

    Added tests

--- src/test/java/com/execution/CmdExecutorCase.java
+++ src/test/java/com/execution/CmdExecutorCase.java
@@ -3,6 +3,8 @@ package com.execution;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class CmdExecutorCase {
 
   private final CmdExecutor cmdExecutor = new CmdExecutor();
@@ -12,4 +14,24 @@ public class CmdExecutorCase {
     cmdExecutor.execute(null);
   }
 
+  @Test
+  public void testExecution() {
+    assertTrue(cmdExecutor.execute("java -version"));
+  }
+
+  @Test
+  public void testExecution1() {
+    assertTrue(cmdExecutor.execute("java -version"));
+    assertTrue(cmdExecutor.execute("java -version"));
+    assertTrue(cmdExecutor.execute("java -version"));
+    assertTrue(cmdExecutor.execute("java -version"));
+    assertTrue(cmdExecutor.execute("java -version"));
+    assertTrue(cmdExecutor.execute("java -version"));
+    assertTrue(cmdExecutor.execute("java -version"));
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void testShutdown() {
+    cmdExecutor.shutdown();
+  }
 }
